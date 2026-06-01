47c06f7175305c47be60bce7e92717172149f1e6
Subhadip Mahata <112509802+subhadiip333@users.noreply.github.com>
2026-06-01 22:52:59 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+                for (char ch : s.toCharArray()) {
+
+                            if (ch == '(' ||
+                                            ch == '[' ||
+                                                            ch == '{') {
+
+                                                                            stack.push(ch);
+
+                                                                                        } else {
+
+                                                                                                        if (stack.isEmpty()) {
+                                                                                                                            return false;
+                                                                                                                                            }
+
+                                                                                                                                                            char top = stack.peek();
+
+                                                                                                                                                                            if ((ch == ')' && top != '(') ||
+                                                                                                                                                                                                (ch == ']' && top != '[') ||
+                                                                                                                                                                                                                    (ch == '}' && top != '{')) {
+
+                                                                                                                                                                                                                                        return false;
+                                                                                                                                                                                                                                                        }
+
+                                                                                                                                                                                                                                                                        stack.pop();
+                                                                                                                                                                                                                                                                                    }
+                                                                                                                                                                                                                                                                                            }
+
+                                                                                                                                                                                                                                                                                                return stack.isEmpty();
+    }
+}
